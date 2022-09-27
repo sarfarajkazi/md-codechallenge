a8b6fd545af4e9c0bd0462edabca40c8875bc80e
sarfarazkazi75 <programmer75.dynamicdreamz@gmail.com>
2022-09-27 10:06:39 +0530

    Add Vendor folder

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'sarfaraz-kazi/library-book-search',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '23b2d82fa60103f4b2b543ffaa92b6025891543d',
+        'type' => 'project',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'sarfaraz-kazi/library-book-search' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '23b2d82fa60103f4b2b543ffaa92b6025891543d',
+            'type' => 'project',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
